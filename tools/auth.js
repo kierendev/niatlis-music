45bf5344d008fb722f490f8d9a1a1ad8df40b45c
Kieren BP <kieren@kieren.nz>
2019-11-15 15:55:30 +1300

    generate correct callback url

--- tools/auth.js
+++ tools/auth.js
@@ -3,7 +3,7 @@ import spotifyApi from './spotify';
 export function redirectToSpotify() {
   const clientId = 'de3ee428dad24e34895545d6a8e697e8';
   const spotifyUrl = 'https://accounts.spotify.com/authorize';
-  const redirectUrl = 'http://localhost:3322/login/callback/';
+  const redirectUrl = `${window.location.origin}/login/callback/`;
   const scope =
     'user-follow-read user-read-recently-played user-top-read user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-read-collaborative playlist-read-private';
   const fullString = `${spotifyUrl}?client_id=${encodeURIComponent(
